f21218fb159095218b4b1c3e857ef6ef2fc6a92c
avigam <avigam@post.bgu.ac.il>
2022-09-20 14:28:18 +0300

    added files

---
+++ R/data.R
@@ -0,0 +1,166 @@
+#' Affective Norms for English Words (AFINN)
+#'
+#' A quanteda dictionary object with the Affective Norms for English Words (AFINN), a publicly available list of English words rated for valence. Contains 2,477 words and phrases with a binary classification into 878 positive (valence > 0) and 1,599 negative (valence < 0) features. From quanteda.dictionaries.'
+#' 
+#' 
+#' @format An object of class dictionary2 of length 2:
+#' 
+#' @source \url{http://www2.imm.dtu.dk/pubdb/views/publication_details.php?id=6010}
+#' 
+#' @references Nielsen, F.Å. (2011). \href{https://arxiv.org/abs/1103.2903}{A new ANEW: Evaluation of a Word List for Sentiment Analysis in Microblogs.} In *Proceedings of the ESWC2011 Workshop on 'Making Sense of Microposts': Big Things Come in Small Packages*, 93–98.
+"AFINN"
+
+
+#' Age of Aquisition Norms for English Words (AoA)
+#'
+#' A publicly available dataset with the Test-based age-of-acquisition norms for 44 thousand English word meanings.
+#' 
+#' 
+#' @format A data frame with 43998 rows and 8 variables:
+#' 
+#' @source \url{https://osf.io/kz2px/}
+#' 
+#' @references Brysbaert, M., Biemiller, A. (2017). \href{https://link.springer.com/article/10.3758/s13428-016-0811-4}{Test-based age-of-acquisition norms for 44 thousand English word meanings} In *Behavior Research Methods* 49, 1520–1523.
+"AoA"
+
+
+#' Concrete Norms for 40 thousand English Word lemmas
+#'
+#' A publicly available dataset with the Test-based age-of-acquisition norms for 44 thousand English word meanings.
+#' 
+#' 
+#' @format A data frame with 39954 rows and 9 variables:
+#' 
+#' @source \url{https://link.springer.com/article/10.3758/s13428-013-0403-5#MOESM1}
+#' 
+#' @references Brysbaert, M., Warriner, A.B. & Kuperman, V. (2014). \href{https://link.springer.com/article/10.3758/s13428-013-0403-5}{Concreteness ratings for 40 thousand generally known English word lemmas} In *Behavior Research Methods* 46, 904–911.
+"concrete"
+
+
+#' The Evaluative Lexicon
+#'
+#' A publicly available dataset with the valence, extremity and emotionality of word lemmas. From the TATE package.
+#' 
+#' 
+#' @format A data frame with 1541 rows and 4 variables:
+#' 
+#' 
+#'  
+#' @references Rocklage, M. D., & Fazio, R. H. (2015). The Evaluative Lexicon: Adjective use as a means of assessing and distinguishing attitude valence, extremity, and emotionality. In *Journal of Experimental Social Psychology*, 56, 214-227.
+"evlex"
+
+
+#' Humor Norms
+#'
+#' A publicly available dataset with humor scores of word lemmas. From the TATE package.
+#' 
+#' 
+#' @format A data frame with 4997 rows and 16 variables:
+#' 
+#'  
+#' @references Engelthaler, T., & Hills, T. T. (2018). Humor norms for 4,997 English words. In *Behavior research methods*, 50(3), 1116-1124.
+"humor"
+
+
+#' The Lancaster Sensorimotor Norms 
+#'
+#' A publicly available dataset with the multidimensional measures of perceptual and action strength for 40,000 English words.
+#' 
+#' 
+#' @format A data frame with 39707 rows and 45 variables:
+#' 
+#' @source \url{https://osf.io/7emr6/}
+#' 
+#' @references Lynott, D., Connell, L., Brysbaert, M. et al. (2020). \href{https://link.springer.com/article/10.3758/s13428-019-01316-z}{The Lancaster Sensorimotor Norms: multidimensional measures of perceptual and action strength for 40,000 English words.} In *Behavior Research Methods* 52, 1271–1291.
+"Lancaster"
+
+
+#' The Mind Perception Dictionary (MPD)
+#'
+#' A quanteda dictionary object with A publicly available dictionary that can assess frequency of words related to mind perception.
+#' 
+#' 
+#' @format An object of class dictionary2 of length 5:
+#' 
+#' @source \url{https://www.adamwaytz.com/measures-and-materials/the-mind-perception-dictionary}
+#' 
+#' @references Schweitzer, S., & Waytz, A. (2021). \href{https://psycnet.apa.org/fulltext/2020-96445-001.pdf}{Language as a window into mind perception: How mental state language differentiates body and mind, human and nonhuman, and the self from others.} In *Journal of Experimental Psychology: General* 150(8), 1642.
+"MPD"
+
+
+#' The Mental-Physical Verb Norms (MPVN)
+#'
+#' A publicly available dataset with the Mental-Physical Verb Norms (MPVN) scores of word lemmas to measure mental state attribution.
+#' 
+#' 
+#' @format A data frame with 250 rows and 6 variables:
+#' 
+#' 
+#' @source \url{https://www.adamwaytz.com/measures-and-materials/the-mind-perception-dictionary}
+#' 
+#' @references Orr, R. I., & Gilead, M. (2022). \href{https://link.springer.com/article/10.3758/s13428-022-01911-7}{Development and validation of the Mental-Physical Verb Norms (MPVN): A text analysis measure of mental state attribution} In *Behavior Research Methods* 1-21.
+"MPVN"
+
+
+
+#' NRC Word-Emotion Association Lexicon
+#'
+#' A quanteda dictionary object with the English version of the NRC Word-Emotion Association Lexicon. The association of words with eight emotions (anger, fear, anticipation, trust, surprise, sadness, joy, and disgust) and two sentiments (negative and positive) manually annotated on Amazon's Mechanical Turk. From quanteda.dictionaries.
+#' 
+#' 
+#' @format An object of class dictionary2 of length 10:
+#' 
+#' 
+#' @source \url{http://saifmohammad.com/WebPages/AccessResource.htm}
+#' 
+#' @references Mohammad, S. & Turney, P. (2013). \href{https://arxiv.org/abs/1308.6297}{Crowdsourcing a Word-Emotion Association Lexicon.} In *Computational Intelligence*, 29(3), 436–465.
+#' 
+#' Mohammad, S. & Turney, P. (2010). \href{https://dl.acm.org/citation.cfm?id=1860635}{Emotions Evoked by Common Words and Phrases: Using Mechanical Turk to Create an Emotion Lexicon.} In *Proceedings of the NAACL-HLT 2010 Workshop on Computational Approaches to Analysis and Generation of Emotion in Text*, June 2010, LA, California.
+"NRC"
+
+
+#' Word prevalence norms for 62,000 English lemmas
+#'
+#' A publicly available dataset with the prevalence measures of 62 thousand english lemmas. Word prevalence refers to the number of people who know the word. The measure was obtained on the basis of an online crowdsourcing study involving over 220,000 people.
+#' 
+#' 
+#' @format A data frame with 61855 rows and 5 variables:
+#' 
+#' @source \url{https://osf.io/g4xrt/}
+#' 
+#' @references  Brysbaert, M., Mandera, P., McCormick, S. F., & Keuleers, E. (2019). \href{https://link.springer.com/article/10.3758/s13428-018-1077-9}{Word prevalence norms for 62,000 English lemmas.} In *Behavior Research Methods* 51(2), 467-479.
+"Prevalences"
+
+
+#' The threat Dictionary
+#'
+#' A quanteda dictionary object with A publicly available dictionary that indexes threat levels from texts.
+#' 
+#' 
+#' @format An object of class dictionary2 of length 1:
+#' 
+#' @source \url{https://www.michelegelfand.com/threat-dictionary}
+#' 
+#' @references Choi, V. K., Shrestha, S., Pan, X., & Gelfand, M. J. (2022). \href{https://www.pnas.org/doi/abs/10.1073/pnas.2113891119}{When danger strikes: A linguistic tool for tracking America’s collective response to threats} In *Proceedings of the National Academy of Sciences* 119(4), 19.
+"threat"
+
+
+#' The Evaluative Lexicon
+#'
+#' A publicly available dataset with the values of valence, arousal and dominance of word lemmas. From the TATE package.
+#' 
+#' 
+#' @format A data frame with 13915 rows and 65 variables:
+#' 
+#'  
+#' @references Warriner, A. B., Kuperman, V., & Brysbaert, M. (2013). Norms of valence, arousal, and dominance for 13,915 English lemmas. In *Behavior research methods*, 45(4), 1191-1207.
+"VAD"
+
+
+
+#' Dictionary Info
+#'
+#' A summary table with the names of dictionaries included in the package, the type of tokens which the dictionary accepts (tokens/lemmas), the type of the dictionary (norms or "simple" like LIWC), the columns of the dataset to use in the text analysis and the default names given to the output of each measure in the dictionary.
+#' 
+#' 
+
---
+++ R/funcs.R
@@ -0,0 +1,160 @@
+
+#' A text analysis Function
+#'
+#' This function allows you to readily analyze texts using a dictionary of choice from the package.
+#' @param data The data set with texts to analyze.
+#' @param text_column The name of the column in data with the texts. Defaults to 'text'.
+#' @param dict_name The name of the dictionary used for the analysis. Defaults to 'concrete'.
+#' 
+#' @keywords lexicon, dictionary
+#' @export
+#' @examples
+#' analyze_text(data.frame(text="Lord, what fools these mortals be!"))
+#' 
+#' analyze_text(data.frame(bard="Lord, what fools these mortals be!"),text_column='bard',dict='VAD')
+
+analyze_text <- function(data,text_column = 'text', dict_name='concrete'){
+  if(sum(dict_info$dict_names%in%dict_name)==0){
+    message('Error, dictionary not found. See dict_info$dict_names for included dictionary names.')
+  }
+  else{
+    dict_entry <- dict_info$dict_entry[dict_info$dict_names%in%dict_name]
+    propped <- preproc_text(data,text_column = text_column,dict_entry=dict_entry)
+    dict_type <- dict_info$dict_type[dict_info$dict_names%in%dict_name]
+    norm_column <- dict_info$norm_column[[which(dict_info$dict_names%in%dict_name)]]
+    out_names <- dict_info$out_norm_column[[which(dict_info$dict_names%in%dict_name)]]
+    dict <- all_dicts[[dict_name]]
+    
+    if(dict_type=='norms'){
+      names(dict)[grep('word',names(dict),ignore.case = T)] <- 'Word'
+      responses <- norm_dicts(propped,dict,column=norm_column)
+      colnames(responses)[2:(1+length(out_names))] <- out_names
+    }
+    else{
+      if(dict_type=='LIWCalike'){
+        responses <- liwcalike_dicts(propped,dict)
+        colnames(responses)[2:(1+length(out_names))] <- out_names
+      }
+    }
+    return(responses)
+  }
+}
+
+#' A function for pre-processing text for analysis
+#'
+#' This function pre-processes a text for analysis by tokenizing or lemmatizing the text.
+#' @param data The data set with texts to analyze.
+#' @param text_column The name of the column in data with the texts. Defaults to 'text'.
+#' @param dict_entry Whether to split text into tokens or lemmas. Defaults to 'tokens'.
+#' 
+#' @keywords lexicon, dictionary
+#' @export
+#' @examples
+#' preproc_text(data.frame(text="For sweetest things turn sourest by their deeds"))
+#' 
+#' preproc_text(data.frame(text="For sweetest things turn sourest by their deeds"),dict_entry='lemmas')
+
+preproc_text <- function(data,text_column = 'text',dict_entry='tokens'){
+  if(dict_entry=='lemmas'){
+      propped <- lemmatize_preproc(data,text_column)
+      return(propped)
+    }
+  else{
+    if(dict_entry=='tokens'){
+      propped <- tokenize_preproc(data,text_column)
+      return(propped)
+      }
+    else{
+      message('Error. Text processing (`dict_entry`) must be either as tokens / lemmas.') 
+      }
+    }
+}
+
+tokenize_preproc <- function(data,text_column,word_column){
+  text_toks <-  data[[text_column]] %>% 
+    quanteda::tokens() %>%
+    quanteda::tokens_tolower()
+  return(text_toks)
+}
+
+lemmatize_preproc <- function(data,text_column,word_column){
+  text_lemmas <- data[[text_column]] %>% 
+    textstem::lemmatize_strings() %>% 
+    quanteda::tokens() %>%
+    quanteda::tokens_tolower()
+  return(text_lemmas)
+}
+
+#' A function for text analysis with norm based dictionaries
+#'
+#' This function performs an analysis on pre-processed text, calculating the levels of the selected norm-based dictionary in the text.
+#' This function isn't limited to dictionaries in the package and can be used with additional custom norm dictionaries (as opposed to the analyze_text function).
+#' @param now_toks The tokens / lemmas from the text pre-processing.
+#' @param now_dict The norms dictionary to use for analysis. Can be fitting dictionaries from the package or any norm dictionary with the format of a data.frame with one column 'Word' which includes tokens / lemmas and other columns providing norm based scores (e.g. concreteness level per word).
+#' @param column The column/columns in the dictionary to be used in the analysis.
+#' @param word_column The column in the dictionary which is the index of tokens / lemmas Defaults to 'Word'.
+#' 
+#' 
+#' @keywords lexicon, dictionary
+#' @export
+#' @examples
+#' proc_toks <- preproc_text(data.frame(text="Uneasy lies the head that wears the crown."))
+#' norm_col <-  dict_info$norm_column[[which(dict_info$dict_names=="concrete")]]
+#' result <- norm_dicts(proc_toks,now_dict=concrete,column=norm_col)
+#' colnames(result)[2] <- dict_info$out_norm_column[[which(dict_info$dict_names=="concrete")]]
+#' result
+#' 
+#' proc_lems <- preproc_text(data.frame(text="Uneasy lies the head that wears the crown."),dict_entry = 'lemmas')
+#' norm_col <-  dict_info$norm_column[[which(dict_info$dict_names=="VAD")]]
+#' result <- norm_dicts(proc_toks,now_dict=VAD,column=norm_col)
+#' colnames(result)[2:4] <- dict_info$out_norm_column[[which(dict_info$dict_names=="VAD")]]
+#' result
+
+
+norm_dicts <- function (now_toks,now_dict,column,word_column='Word'){
+  dict <- now_dict[,c(word_column,column)]
+  
+  all_df <- tibble(docname=names(now_toks))
+  now_words <- dict[[word_column]] %>% tolower()
+  for (i in (1:(length(dict)-1))) {
+    now_norms <- dict[[column[i]]]
+    all_m_count <- numeric(length(now_toks))
+    for (tok_i in 1:length(now_toks)) {
+      now_m_count <- mean(now_norms[now_words%in%now_toks[[tok_i]]],na.rm=T)
+      all_m_count[tok_i] <- now_m_count
+    }
+    now_df <- tibble(V1=all_m_count)
+    colnames(now_df) <- paste0('m_',column[i])
+    all_df <- cbind(all_df,now_df)
+  }
+  
+  return(all_df)
+}    
+
+
+
+#' A function for text analysis with simple count dictionaries
+#'
+#' This function performs an analysis on pre-processed text, calculating the proportions of words per dictionary in the text.
+#' This function isn't limited to dictionaries in the package and can be used with additional custom dictionaries (as opposed to the analyze_text function).
+#' @param now_toks The tokens from the text pre-processing.
+#' @param now_dict The simple ("LIWCalike") dictionary to use for analysis. Can be fitting dictionaries from the package or any dictionary with the format of a quanteda::dictionary() (e.g. a dictionary of words that represent negative emotion).
+#' 
+#' @keywords lexicon, dictionary
+#' @export
+#' @examples
+#' 
+#' proc_toks <- preproc_text(data.frame(text="I wasted time, and now doth time waste me."))
+#' liwcalike_dicts(proc_toks,AFINN)
+#' 
+#' 
+
+
+liwcalike_dicts <- function (now_toks,now_dict){
+  all_count <- quanteda::dfm_lookup(quanteda::dfm(now_toks),dictionary = now_dict)/quanteda::ntoken(now_toks)*100
+  all_df <- quanteda::convert(all_count,'data.frame') %>% as_tibble()
+  colnames(all_df)[1] <- 'docname'
+  return(all_df)
+}    
+
+
